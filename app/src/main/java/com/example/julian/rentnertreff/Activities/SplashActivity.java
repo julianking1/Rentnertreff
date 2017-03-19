a089ba85be7d02e989e4d90c213cbf90297085ee
Julian <julian_koenig96@gmx.de>
2017-03-19 22:36:14 +0100

    Event-Class and DatabaseHAndler

---
+++ app/src/main/java/com/example/julian/rentnertreff/Activities/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.julian.rentnertreff.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.julian.rentnertreff.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2500);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
