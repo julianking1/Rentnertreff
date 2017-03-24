1c6ee07ce0f125ec655952916f8a141f88a1b351
JanaPain <kuntz.jana@googlemail.com>
2017-03-24 14:54:27 +0100

    Tada. I have no idea what I'm doing :D

--- app/src/main/java/com/example/julian/rentnertreff/Fragments/ListFragment.java
+++ app/src/main/java/com/example/julian/rentnertreff/Fragments/ListFragment.java
@@ -1,6 +1,7 @@
 package com.example.julian.rentnertreff.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -21,9 +22,8 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment{
 
-    View view;
     List<Event> event_list;
     ArrayList<String> title_list;
 
@@ -34,7 +34,7 @@ public class ListFragment extends Fragment {
     public void setList(List<Event> list){
         this.event_list = list;
     }
-
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,9 +45,9 @@ public class ListFragment extends Fragment {
         title_list = new ArrayList<String>();
 
         //add each title to arraylist
-        for (Event e : event_list) {
-            title_list.add(e.getTitle());
-        }
+        //for (Event e : event_list) {
+        //    title_list.add(e.getTitle());
+        //}
 
 
         ListView lv = (ListView) view.findViewById(R.id.listView_events);
@@ -59,9 +59,8 @@ public class ListFragment extends Fragment {
         //lv.setAdapter(arrayAdapter);
 
 
-
-
         return view;
     }
+
 
 }
