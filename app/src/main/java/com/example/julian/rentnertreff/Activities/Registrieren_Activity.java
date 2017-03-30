f1c49cfdf3bf346f0cf174f8d3ca923b3bf59624
Julian <julian_koenig96@gmx.de>
2017-03-30 13:59:16 +0200

    demnächst noch x Tage

--- app/src/main/java/com/example/julian/rentnertreff/Activities/Registrieren_Activity.java
+++ app/src/main/java/com/example/julian/rentnertreff/Activities/Registrieren_Activity.java
@@ -17,23 +17,23 @@ public class Registrieren_Activity extends AppCompatActivity {
     private SeekBar umkreisControl;
     private TextView umkreisView;
     private Button button;
+    private TextView fehlermeldung;
+
+    private EditText email;
+    private EditText password;
+    private EditText wohnort;
+    private EditText name;
+    private int progressChanged;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrieren);
 
-        button = (Button) findViewById(R.id.button_register_registrieren);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Registrieren_Activity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
 
         umkreisView = (TextView) findViewById(R.id.textView_umkreis);
+
+
 
         umkreisControl = (SeekBar) findViewById(R.id.seekBar_umkreis);
         umkreisControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -54,7 +54,81 @@ public class Registrieren_Activity extends AppCompatActivity {
 
             }
         });
+
+        button = (Button) findViewById(R.id.button_register_registrieren);
+        name = (EditText) findViewById(R.id.edit_register_name);
+        wohnort =  (EditText) findViewById(R.id.edit_register_wohnort) ;
+        email = (EditText) findViewById(R.id.edit_register_mail) ;
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (proofName(name.getText().toString()) && proofMail(email.getText().toString()) && proofPassword(password.getText().toString()) && proofLocation(wohnort.getText().toString())  && proofSurrounding(progressChanged)){
+                    Intent intent = new Intent(Registrieren_Activity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
+
+
+    public boolean proofMail(String email) {
+        fehlermeldung = (TextView) findViewById(R.id.register_fehlermeldung);
+        if (email.contains("@")){
+            return true;
+        }else {
+            fehlermeldung.setText("e-Mail Adresse muss ein @ enthalten");
+            return false;
+        }
+    }
+
+
+    public boolean proofPassword (String password){
+        fehlermeldung = (TextView) findViewById(R.id.register_fehlermeldung);
+
+        if (password.length() > 6 ){
+            return true;
+        }else {
+            fehlermeldung.setText("Passwort muss mindestens 6 Zeichen lang sein!");
+            return false;
+        }
+    }
+
+    public boolean proofName (String name){
+        fehlermeldung = (TextView) findViewById(R.id.register_fehlermeldung);
+
+        if (name.length() > 0 ){
+            return true;
+        }else {
+            fehlermeldung.setText("Name darf nicht leer sein!");
+            return false;
+        }
+    }
+
+    public boolean proofLocation (String location){
+        fehlermeldung = (TextView) findViewById(R.id.register_fehlermeldung);
+
+        if (location.length() > 0 ){
+            return true;
+        }else {
+            fehlermeldung.setText("Name darf nicht leer sein!");
+            return false;
+        }
+    }
+
+    public boolean proofSurrounding (int surronding){
+        fehlermeldung = (TextView) findViewById(R.id.register_fehlermeldung);
+
+        if (surronding > 0 ){
+            return true;
+        }else {
+            fehlermeldung.setText("Ein Umkreis von unter 1 km ist nicht empfehlenswert!");
+            return false;
+        }
+    }
+
+
+
 
 
 
